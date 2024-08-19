6298e682bc6f6ed2ecae74e4c5e5ac1faf692ac8
NamDo8467 <huynhnamdo@gmail.com>
2024-08-18 20:34:08 -0400

    modified kill zone

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,12 +2,11 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_body_entered(body):
-	print("You die")
-	
-	timer.start()
-	
-
-
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
+
+
+func _on_body_entered(body):
+	print("You die")
+	print(body)
+	timer.start()
