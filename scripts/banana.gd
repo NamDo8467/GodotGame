080154385d45204e4c3ada6ae889bfcbe7561bab
NamDo8467 <huynhnamdo@gmail.com>
2024-08-18 20:32:37 -0400

    added collected animation for banana

--- scripts/banana.gd
+++ scripts/banana.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+@onready var banana = $AnimatedSprite2D
 
 func _on_body_entered(body):
-	queue_free()
+	banana.play("collected")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if banana.animation == "collected":
+		queue_free()
