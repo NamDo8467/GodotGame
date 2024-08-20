d3deefa014966f2bc6add7e20e33c5103091aa33
NamDo8467 <huynhnamdo@gmail.com>
2024-08-19 22:40:36 -0400

    change killzone specification

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_timer_timeout():
-	get_tree().reload_current_scene()
-
 
 func _on_body_entered(body):
 	print("You die")
-	print(body)
+	Engine.time_scale = 0.8
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
+	
+	
+func _on_timer_timeout():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
+
+
+
