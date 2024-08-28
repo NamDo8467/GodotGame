c97d1e979288006f491d57db3be7948b95c97fed
NamDo8467 <huynhnamdo@gmail.com>
2024-08-28 10:49:33 -0400

    change jump ability

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 250.0
-const JUMP_VELOCITY = -550.0
+const JUMP_VELOCITY = -380.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
