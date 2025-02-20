eba0b1f43b768f4bc0355cf041390cc60e85227d
NamDo8467 <huynhnamdo@gmail.com>
2025-02-20 14:58:13 -0500

    probably remove some spaces in the code

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -1,12 +1,12 @@
 extends Node2D
-@onready var scene_transition_animation = $SceneTransitionAnimation/AnimationPlayer
+#@onready var scene_transition_animation = $SceneTransitionAnimation/AnimationPlayer
+@onready var scene_transition_animation = SceneTransitionAnimation
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	scene_transition_animation.get_parent().get_node("ColorRect").color.a = 255
-	#await get_tree().create_timer(0.5).timeout
-	scene_transition_animation.play("fade_out")	
+	scene_transition_animation.color_rect.color.a = 255
+	scene_transition_animation.animation_player.play("fade_out")	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
