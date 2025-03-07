a5a6c769798dd8fb3f40d899da5689534a404178
BiggyBruhmanz <93736992+BiggyBruhmanz@users.noreply.github.com>
2025-03-07 16:51:08 -0500

    Knife Movement

--- scenes/Minigames/Cutting_Minigame.gd
+++ scenes/Minigames/Cutting_Minigame.gd
@@ -1,30 +1,42 @@
 extends Node2D
 
-@onready var Knife = $CanvasLayer/Knife
-@onready var P1_Pos = Vector2(0,0)
-@onready var P2_Pos = Vector2(0,0)
+@onready var knife = $CanvasLayer/Knife
+@onready var p1 = $CanvasLayer/Player_1
+@onready var p2 = $CanvasLayer/Player_2
+
+var BASE_POSITION = Vector2(40,300)
+
+var player_Speed = 1 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Knife.position = Vector2(40, 300)
+	knife.position = BASE_POSITION
+	p1.position = Vector2(BASE_POSITION.x, 64)
+	p2.position = Vector2(BASE_POSITION.x, 536)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_P1()
-	move_P2()
+	Move_P1()
+	Move_P2()
 	
-	move_Knife()
+	Move_Knife()
 
-func move_P1():
+func Move_P1():
 	if Input.is_action_pressed("P1_Minigame_Move_Right"):
-		P1_Pos.x += 1
+		p1.position.x += player_Speed
+	if Input.is_action_pressed("P1_Minigame_Move_Left"):
+		p1.position.x -= player_Speed
 
-func move_P2():
+
+func Move_P2():
 	if Input.is_action_pressed("P2_Minigame_Move_Right"):
-		P2_Pos.x += 1
+		p2.position.x += player_Speed
+	if Input.is_action_pressed("P2_Minigame_Move_Left"):
+		p2.position.x -= player_Speed
 
-func move_Knife():
-	Knife.position.x = (P1_Pos.x + P2_Pos.x) / 2 # position of centre of knife equal to half the distance between them
+func Move_Knife():
+	knife.position.x = (p1.position.x + p2.position.x) / 2 # position of centre of knife equal to half the distance between them
 	
-
-
+	var Hypotenuse = p1.position.distance_to(knife.position)
+	var Adjacent = (p1.position.x - p2.position.x) / 2
+	knife.rotation = PI/2 - acos(Adjacent / Hypotenuse)
