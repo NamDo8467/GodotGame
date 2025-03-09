243a12ed0e7b06a018323e9edf80fec23b6b5ac7
NamDo8467 <huynhnamdo@gmail.com>
2025-03-08 21:49:03 -0500

    added logic to spawn player 2

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 
 var player1_scene = preload("res://scenes/player.tscn")
-var starting_position = Vector2(787,888)
+var player2_scene = preload("res://scenes/player2.tscn")
+var player1_starting_position = Vector2(787,888)
+var player2_starting_position = Vector2(928, 888)
 var player1 = null
+var player2 = null
 
 func _process(_delta):
 	if player1 == null:
 		player1 = player1_scene.instantiate()
-		player1.position = starting_position
+		player1.position = player1_starting_position
 		get_parent().add_child(player1)
+	if player2 == null:
+		player2 = player2_scene.instantiate()
+		player2.position = player2_starting_position
+		get_parent().add_child(player2)
+		
