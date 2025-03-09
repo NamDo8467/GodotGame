20a25ac663de584f67c81ba07d443d92fe09ab07
NamDo8467 <huynhnamdo@gmail.com>
2025-03-09 10:15:40 -0400

    changed the starting positions of both players

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 var player1_scene = preload("res://scenes/player.tscn")
 var player2_scene = preload("res://scenes/player2.tscn")
-var player1_starting_position = Vector2(787,888)
-var player2_starting_position = Vector2(928, 888)
+var player1_starting_position = Vector2(-89,927)
+var player2_starting_position = Vector2(-150, 927)
 var player1 = null
 var player2 = null
 
