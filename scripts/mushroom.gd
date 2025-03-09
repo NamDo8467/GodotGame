06cdfa0802fdd94715870f78498183903bcbb03a
NamDo8467 <huynhnamdo@gmail.com>
2025-03-08 21:49:24 -0500

    comment out code

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -12,13 +12,14 @@ var direction = -1 # currently going to the left
 	#mushroom.flip_h = true
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if ray_cast_right.is_colliding() or position.x >= 550:
-		direction = -1
-		mushroom.flip_h = false
-	elif ray_cast_left.is_colliding() or position.x <= 100:
-		direction = 1
-		mushroom.flip_h = true
-	
-	position.x += direction * SPEED * delta
+#func _process(delta):
+	#pass
+	#if ray_cast_right.is_colliding() or position.x >= 550:
+		#direction = -1
+		#mushroom.flip_h = false
+	#elif ray_cast_left.is_colliding() or position.x <= 100:
+		#direction = 1
+		#mushroom.flip_h = true
+	#
+	#position.x += direction * SPEED * delta
 
