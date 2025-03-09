3caced613bbd30d2b372e1e67864a2c92a3b74e4
NamDo8467 <huynhnamdo@gmail.com>
2025-03-08 21:47:38 -0500

    added second player and the logic to collect the trampoline

--- scripts/trampoline.gd
+++ scripts/trampoline.gd
@@ -4,6 +4,7 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 @onready var timer = $Timer
 @onready var trampoline_animation = $AnimatedSprite2D
+var picked_up = false
 
 #timer.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,9 +13,19 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	trampoline_animation.play("push")
-	var player = body
-	player.velocity.y = -650
+	if picked_up == true:
+		trampoline_animation.play("push")
+		var player = body
+		player.velocity.y = -650
+	else:
+		#print(body)
+		#print(self)
+		picked_up = true
+		body.add_to_weapon_list(self)
+		self.position = Vector2(0,0)
+		get_parent().call_deferred("remove_child", self)
+		
+		
 	
 	
 	
