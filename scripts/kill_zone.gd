4e8bb8c15aa4e9ecda037160105c1d271e4597fb
NamDo8467 <huynhnamdo@gmail.com>
2025-03-09 10:15:13 -0400

    modified kill zone functionality

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 @onready var timer = $Timer
 var player = null
-func _on_body_entered(body):	
-	body.get_node("CollisionShape2D").queue_free()
-	player = body
-	timer.start()
+func _on_body_entered(body):
+	if body.name == "player" or body.name == "player2":
+		player = body
+		body.get_node("CollisionShape2D").queue_free()
+		timer.start()
 
 func _on_timer_timeout():
 	player.queue_free()
