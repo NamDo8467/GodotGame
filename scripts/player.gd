bbb0bc7ee94f0c8a35eff536676d1baedd9dd5f7
NamDo8467 <huynhnamdo@gmail.com>
2025-03-08 21:48:33 -0500

    added the logic to pick up weapon and change weapon

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -380.0
-
+var weapon_list = []
+var current_weapon_index = -1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -47,6 +48,21 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+	
+	# Handle choosing weapon
+	if Input.is_action_pressed("choose_weapon"):
+		if len(weapon_list) > 0:
+			if current_weapon_index + 1 >= len(weapon_list):
+				remove_child(weapon_list[current_weapon_index])
+				current_weapon_index = -1
+			else:
+				current_weapon_index = current_weapon_index + 1
+				var new_weapon = weapon_list[current_weapon_index]
+				new_weapon.position = Vector2(-38,-34)
+				add_child(new_weapon)
+			
+func add_to_weapon_list(weapon):
+	weapon_list.append(weapon)
 
 
 
