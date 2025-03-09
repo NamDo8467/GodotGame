871291386e9fe0707c9d9cb4da3765e6d340e55a
BiggyBruhmanz <93736992+BiggyBruhmanz@users.noreply.github.com>
2025-03-08 23:37:45 -0500

    A bunch of code for the Cutting Stuff

--- scenes/Minigames/Cutting_Minigame.gd
+++ scenes/Minigames/Cutting_Minigame.gd
@@ -8,20 +8,45 @@ var BASE_POSITION = Vector2(40,300)
 
 var player_Speed = 1 
 
+
+# Knife Cutting Variables
+var isCutting = false
+var original_Knife_Scale
+var knife_Shrink_Percent = 0.95
+var knife_Shirnk_Time = 0.6 # Total Time /2 for going down then back up
+
+# Cutting Line Variables
+var MIN_CUTTING_ZONE = 160 # Arbitrary
+var MAX_CUTTING_ZONE = 610 # Arbitrary
+var BASE_CUTTING_ZONE = 450
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Create_CutLines()
+	
 	knife.position = BASE_POSITION
 	p1.position = Vector2(BASE_POSITION.x, 64)
 	p2.position = Vector2(BASE_POSITION.x, 536)
+	
+	original_Knife_Scale = knife.scale
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("Knife Rotation: " + str(knife.rotation))
+	if (Can_Move()):
+		Move_P1()
+		Move_P2()
+		Move_Knife()
+	#print("P1 Position: " + str(p1.position))
+	#print("P2 Position: " + str(p2.position))
+	#print("Knife Rotation: " + str(knife.rotation))
 	
-	Move_P1()
-	Move_P2()
-	
-	Move_Knife()
+	Cut_Knife()
+
+func Can_Move():
+	if isCutting: return false
+	if isCutting: return false
+	return true
 
 func Move_P1():
 	if Input.is_action_pressed("P1_Minigame_Move_Right") && knife.rotation < 0.3:
@@ -47,3 +72,27 @@ func Move_Knife():
 	
 	if (knife.rotation < -0.3):
 		knife.rotation = -0.3
+
+func Cut_Knife():
+	if Input.is_action_pressed("P1_Minigame_Action_1") && !isCutting:
+		var tween = get_tree().create_tween()
+		
+		isCutting = true
+		
+		tween.tween_property(knife, "scale", original_Knife_Scale * knife_Shrink_Percent, knife_Shirnk_Time/2)
+		tween.tween_property(knife, "scale", original_Knife_Scale, knife_Shirnk_Time/2).finished.connect(set.bind("isCutting", false))
+
+
+func Create_CutLines():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var num_CutLines = rng.randi_range(5, 6)
+	var tmp_Position = Vector2(MIN_CUTTING_ZONE, 0)
+	var tmp_Angle = 0
+	
+	for x in num_CutLines:
+		rng.randomize()
+		tmp_Position
+		rng.randomize()
+		tmp_Angle = rng.rand_range(-0.3, 0.3)
+		# create them
