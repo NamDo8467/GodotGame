7c926d38ee3bcfeb2535012cfa3f084cb289645e
NamDo8467 <huynhnamdo@gmail.com>
2025-03-10 17:49:19 -0400

    figured out the ratio when moving the elevator up and the spawning are which is 1:0.4 and added buttons function to move elevator up and down

--- scripts/elevator.gd
+++ scripts/elevator.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_UP) or Input.is_joy_button_pressed(0, JOY_BUTTON_RIGHT_SHOULDER):
