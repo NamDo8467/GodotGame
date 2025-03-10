bd170b61c7a7959d3b92cdcc91a389cedf8be02a
NamDo8467 <huynhnamdo@gmail.com>
2025-03-10 17:47:09 -0400

    figured out the ratio when moving the elevator up and the spawning are which is 1:0.16

--- scripts/elevator.gd
+++ scripts/elevator.gd
@@ -7,11 +7,11 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_UP) or Input.is_joy_button_pressed(0, JOY_BUTTON_RIGHT_SHOULDER):
-		if self.position.y > -570:
-			self.position.y -= 2
-			Global.spawning_position_y -= 0.8
+		if self.position.y > -590:
+			self.position.y -= 4
+			Global.spawning_position_y -= 1.6
 		
 	elif Input.is_key_pressed(KEY_DOWN) or Input.is_joy_button_pressed(0, JOY_BUTTON_LEFT_SHOULDER):
 		if self.position.y < 2:
-			self.position.y += 2
-			Global.spawning_position_y += 0.8
+			self.position.y += 4
+			Global.spawning_position_y += 1.6
