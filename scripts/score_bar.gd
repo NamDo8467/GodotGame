7fd9807f21720e2455875653d60f60437bc993ea
BiggyBruhmanz <93736992+BiggyBruhmanz@users.noreply.github.com>
2025-03-11 02:35:52 -0400

    Added a thank you text at the end

--- scripts/score_bar.gd
+++ scripts/score_bar.gd
@@ -1,5 +1,7 @@
 extends ProgressBar
 
+@onready var text_Thank_You = $Thank_You_Text #PlaceHolder for playetest
+
 var score_from_minigames = Global.score_from_minigames
 var serving_score = 0.0
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +18,9 @@ func _ready():
 func _process(delta):
 	if value < score_from_minigames:
 		value += 30 * delta
+	
+	if value >= score_from_minigames:
+		text_Thank_You.visible = true
 
 #func Minigame_Finished(score):
 	#score_from_minigames.append(score)
