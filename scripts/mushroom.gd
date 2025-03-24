48b38ee58124b047de237b233cf69094b743b434
NamDo8467 <huynhnamdo@gmail.com>
2025-03-23 20:18:26 -0400

    commented out code

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 const SPEED = 65
-@onready var ray_cast_right = $RayCastRight
-@onready var ray_cast_left = $RayCastLeft
+#@onready var ray_cast_right = $RayCastRight
+#@onready var ray_cast_left = $RayCastLeft
 @onready var mushroom = $AnimatedSprite2D
 @onready var kill_zone = $KillZone
 
-var direction = -1 # currently going to the left
+#var direction = -1 # currently going to the left
 
 #func _ready():
 	#mushroom.flip_h = true
