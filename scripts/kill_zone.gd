91fb2455731059290b0306da5680986930169c7b
NamDo8467 <huynhnamdo@gmail.com>
2025-03-27 22:57:38 -0400

    kill players when they are being picked up

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,15 +1,24 @@
 extends Area2D
 
 @onready var timer = $Timer
-var player = null
+var player1 = null
+var player2 = null
 func _on_body_entered(body):
-	if body.name == "Player" or body.name == "Player2":
-		player = body
+	if body.name == "Player" :
+		player1 = body
+		body.get_node("CollisionShape2D").queue_free()	
+	if body.name == "Player2":
+		player2 = body
 		body.get_node("CollisionShape2D").queue_free()
-		timer.start()
+	
+	
+	timer.start()
 
 func _on_timer_timeout():
-	player.queue_free()		
+	if player1:
+		player1.queue_free()	
+	if player2:
+		player2.queue_free()	
 
 
 
