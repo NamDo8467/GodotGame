928f44192087aedc4cb7b3492479517c34383a7e
NamDo8467 <huynhnamdo@gmail.com>
2025-03-28 12:28:49 -0400

    fixed bugs on freed object can not be used

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -11,14 +11,26 @@ func _on_body_entered(body):
 		player2 = body
 		body.get_node("CollisionShape2D").queue_free()
 	
-	
+	if body.position.x <= 694:
+		Global.die_outside_of_the_room = true
+		Global.spawning_position_x = 130
+	else:
+		Global.die_outside_of_the_room = false
+		Global.spawning_position_x = 1053
 	timer.start()
 
 func _on_timer_timeout():
 	if player1:
-		player1.queue_free()	
+		if player1.has_node("Trampoline"):
+			player1.remove_child(player1.get_node("Trampoline"))
+			
+		player1.queue_free()
+		player1 = null	
 	if player2:
-		player2.queue_free()	
+		if player2.has_node("Trampoline"):
+			player2.remove_child(player2.get_node("Trampoline"))
+		player2.queue_free()
+		player2 = null	
 
 
 
