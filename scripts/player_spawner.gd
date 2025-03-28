f140b3512552cc08cca0553ac39efdf441a4c3eb
NamDo8467 <huynhnamdo@gmail.com>
2025-03-28 12:27:42 -0400

    added logic to spawn players in the room or outside the room

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 var player1_scene = preload("res://scenes/player.tscn")
 var player2_scene = preload("res://scenes/player2.tscn")
-var player1_starting_position = Vector2(130,Global.spawning_position_y)
-var player2_starting_position = Vector2(10, Global.spawning_position_y)
+#var player1_starting_position = Vector2(Global.spawning_position_x, Global.spawning_position_y)
+#var player2_starting_position = Vector2(Global.spawning_position_x - 120, Global.spawning_position_y)
 var player1 = null
 var player2 = null
 
@@ -14,11 +14,11 @@ func _ready():
 func _process(_delta):
 	if player1 == null:
 		player1 = player1_scene.instantiate()
-		player1_starting_position.y = Global.spawning_position_y
+		var player1_starting_position = Vector2(Global.spawning_position_x, Global.spawning_position_y)
 		player1.position = player1_starting_position
 		get_parent().add_child(player1)
 	if player2 == null:
 		player2 = player2_scene.instantiate()
-		player2_starting_position.y = Global.spawning_position_y
+		var player2_starting_position = Vector2(Global.spawning_position_x - 120, Global.spawning_position_y)
 		player2.position = player2_starting_position
 		get_parent().add_child(player2)	
