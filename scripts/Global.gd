1c8466a93fc7b4f446be60b664cc8174e40bf67a
NamDo8467 <huynhnamdo@gmail.com>
2025-03-28 22:02:50 -0400

    added floor detection

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -36,6 +36,15 @@ var die_outside_of_the_room = false
 # set of players to transition to the elevator section
 var player_set_to_show_elevator = {}
 
+# Total number of floor levels
+var total_floor_level = 4
+var current_floor = 1
+func go_up_one_floor_level():
+	current_floor += 1
+	
+func go_down_one_floor_level():
+	current_floor -= 1
+	
 func add_score():
 	score += 1
 	
