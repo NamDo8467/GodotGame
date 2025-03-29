f9c79c4b68ee666d704c1b76e126ef6dc429922d
NamDo8467 <huynhnamdo@gmail.com>
2025-03-28 22:02:23 -0400

    changed the walking boundary of the fish

--- scripts/fish_monster.gd
+++ scripts/fish_monster.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 const SPEED = 65
 @onready var fish_monster = $AnimatedSprite2D
-
+var right_walking_boundary = 1120
+var left_walking_boundary = 864
+var parent_name: String
 var direction = 1 # currently going to the right
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	var parent_name = get_parent().name
+	if parent_name == "SecondFloor":
+		right_walking_boundary = 1500
+	elif parent_name == "ThirdFloor":
+		right_walking_boundary = 1120
+		
 func _process(delta):
-	if position.x >= 1120:
+	#print(get_parent().name)
+	if position.x >= right_walking_boundary:
 		direction = -1
 		fish_monster.flip_h = true
 	elif position.x <= 864:
