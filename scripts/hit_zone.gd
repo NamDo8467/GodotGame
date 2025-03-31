6a60f53d2e7fee5a2ef186ef7b0f6c2b5ca9f123
NamDo8467 <huynhnamdo@gmail.com>
2025-03-30 22:53:21 -0400

    everytime the players hit a monster, sound effect will playe

--- scripts/hit_zone.gd
+++ scripts/hit_zone.gd
@@ -1,19 +1,26 @@
 extends Area2D
 
+@onready var squished_sound: AudioStreamPlayer2D = $SquishedSound
 @onready var timer = $Timer
 @onready var monster = get_parent()
 func _on_body_entered(body):
+	monster.get_node("KillZone").queue_free()
+	squished_sound.play()
+	await squished_sound.finished
 	if monster.name == "CabbageMonster":
 		monster.queue_free()
 		Global.transition_to_minigames()
 	else:
 		monster.queue_free()
-	
+	#monster.get_node("KillZone").queue_free()
 	#timer.start()
+	
 	
 func _on_timer_timeout():
 	pass
-	#SceneTransitionAnimation.change_scene()
-		#
-	#await SceneTransitionAnimation.animation_player.animation_finished
-	#get_tree().change_scene_to_file("res://scenes/Minigames/Minigame_Menu/Minigame_Menu.tscn")
+	#if monster.name == "CabbageMonster":
+		#monster.queue_free()
+		#Global.transition_to_minigames()
+	#else:
+		#monster.queue_free()
+	
