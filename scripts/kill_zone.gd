290b376be4b5742adac0c72cbef4838cbdf38302
NamDo8467 <huynhnamdo@gmail.com>
2025-03-31 23:50:12 -0400

    added logic to play falling sound and queue free the players after falling down

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -6,12 +6,20 @@ var player2 = null
 func _on_body_entered(body):
 	if body.name == "Player" :
 		player1 = body
-		body.get_node("CollisionShape2D").queue_free()	
+		var collision_shape = body.get_node("CollisionShape2D")
+		collision_shape.queue_free()
+		var falling_sound = player1.get_node("FallingSound")
+		falling_sound.play()
+		#await falling_sound.finished
+			
 	if body.name == "Player2":
 		player2 = body
 		body.get_node("CollisionShape2D").queue_free()
-	
-	if body.position.x <= 694:
+		var falling_sound = player2.get_node("FallingSound")
+		falling_sound.play()
+		#await falling_sound.finished
+		
+	if body.position.x <= 900:
 		Global.die_outside_of_the_room = true
 		Global.spawning_position_x = 130
 	else:
@@ -23,14 +31,12 @@ func _on_timer_timeout():
 	if player1:
 		if player1.has_node("Trampoline"):
 			player1.remove_child(player1.get_node("Trampoline"))
-			
 		player1.queue_free()
-		player1 = null	
+		player1 = null
 	if player2:
 		if player2.has_node("Trampoline"):
 			player2.remove_child(player2.get_node("Trampoline"))
 		player2.queue_free()
 		player2 = null	
-
 
 
