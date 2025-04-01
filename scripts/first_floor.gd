75b3b58b32e3a2267ae92fd0febc3b673bbb4f18
NamDo8467 <huynhnamdo@gmail.com>
2025-04-01 11:55:37 -0400

    added a script to detect whether the trampoline has been picked

---
+++ scripts/first_floor.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Global.is_trampoline_picked_up == true:
+		get_node("Trampoline").queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
