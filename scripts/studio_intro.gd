03a6c4b81b453485c15deaac54eb4381ffe3d45f
NamDo8467 <huynhnamdo@gmail.com>
2025-04-03 20:56:08 -0400

    added studio intro scene

---
+++ scripts/studio_intro.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@onready var animation_player:AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("fade_in")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+	
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
