63aa700e6e9203b724d8cb909bdee7b1d95a236e
NamDo8467 <huynhnamdo@gmail.com>
2025-04-08 09:27:03 -0400

    added logic to play different music

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -5,17 +5,22 @@ const title_screen_background_music = preload("res://assets/Audio/level_backgrou
 
 const level_background_music = preload("res://assets/Audio/title_screen_background_sound.mp3")
 
+var current_music = null
 
-func play_music(music: AudioStream, volume=0.0):
-	if stream == music:
+func play_music(music: AudioStream,  type:String, volume=0.0):
+	if current_music == type:
 		return
+	#if stream == music:
+		#return
 		
 	stream = music
 	volume_db = volume
 	play()
 	
 func play_title_screen_music():
-	play_music(title_screen_background_music, 22)
+	play_music(title_screen_background_music, "title_screen", 22)
+	current_music = "title_screen"
 	
 func play_level_music():
-	play_music(level_background_music, 24)
+	play_music(level_background_music, "level", 0)
+	current_music = "level"
