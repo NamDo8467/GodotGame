9fbc3d09eba3dded19d96fe23036b00adcf98846
NamDo8467 <huynhnamdo@gmail.com>
2025-04-08 09:27:28 -0400

    play level music at the beginning

--- scripts/first_floor.gd
+++ scripts/first_floor.gd
@@ -6,7 +6,7 @@ func _ready():
 	if Global.is_trampoline_picked_up == true:
 		get_node("Trampoline").queue_free()
 
-
+	AudioPlayer.play_level_music()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
