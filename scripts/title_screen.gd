1aaa0c5add578c829c05de4ac7c21e7f74b7cf2e
NamDo8467 <huynhnamdo@gmail.com>
2025-04-08 09:00:47 -0400

    removed audiostreamplayer and added a global audio player

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	AudioPlayer.play_title_screen_music()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,3 +16,7 @@ func _on_quit_button_pressed():
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/settings_scene.tscn")
+
+
+func _on_about_us_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/about_scene.tscn")
