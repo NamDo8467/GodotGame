4cbc3b531bdf5fbc2d1dccf4ff8fa0b44d85032c
NamDo8467 <huynhnamdo@gmail.com>
2025-05-01 12:01:47 -0400

    configure volume slider to change the volume of the game

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -3,27 +3,35 @@ extends AudioStreamPlayer
 
 const title_screen_background_music = preload("res://assets/Audio/level_background_music.wav")
 
-const level_background_music = preload("res://assets/Audio/title_screen_background_sound.mp3")
+const level_background_music = preload("res://assets/Audio/title_screen_background_sound.wav")
 
 var current_music = null
 
-func play_music(music: AudioStream,  type:String, volume=0.0):
+var global_volume = 5
+
+var current_volume_slider_value = 100
+
+func play_music(music: AudioStream,  type:String):
 	if current_music == type:
 		return
 	#if stream == music:
 		#return
 		
 	stream = music
-	volume_db = volume
+	volume_db = global_volume
 	play()
 	
 func play_title_screen_music():
-	play_music(title_screen_background_music, "title_screen", 15)
+	play_music(title_screen_background_music, "title_screen")
 	current_music = "title_screen"
 	
 func play_level_music():
-	play_music(level_background_music, "level", 0)
+	play_music(level_background_music, "level")
 	current_music = "level"
 
 func Stop_All_Music():
 	self.stop()
+	
+func change_music_volume(volume: int):
+	volume_db = volume
+	global_volume = volume
